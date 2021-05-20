96b882848331f3e7a93b9eab169264ec4787e8a7
Michal S <michalsienczuk@gmail.com>
2021-05-20 17:41:27 +0200

    add server

--- client/app.js
+++ client/app.js
@@ -1,12 +1,45 @@
-'use strict';
-
-const references = {
-  loginForm: document.getElementById('welcome-form'),
-  messagesSection: document.getElementById('messages-section'),
-  messagesList: document.getElementById('messages-list'),
-  addMessageForm: document.getElementById('add-messages-form'),
-  userNameInput: document.getElementById('username'),
-  messageContentInput: document.getElementById('message-content')
-};
+const loginForm = document.getElementById("welcome-form");
+const messagesSection = document.getElementById("messages-section");
+const messagesList = document.getElementById("messages-list");
+const addMessageForm = document.getElementById("add-messages-form");
+const userNameInput = document.getElementById("username");
+const messageContentInput = document.getElementById("message-content");
 
 let userName = "";
+
+function login(event) {
+  event.preventDefault();
+  if (userNameInput.value) {
+    userName = userNameInput.value;
+    loginForm.classList.remove("show");
+    messagesSection.classList.add("show");
+  } else {
+    alert("this field cannot be empty");
+  }
+}
+function sendMessage(event) {
+  event.preventDefault();
+  if (messageContentInput.value) {
+    addMessage(userName, messageContentInput.value);
+
+    messageContentInput.value = "";
+  } else {
+    alert("this field cannot be empty");
+  }
+}
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
+loginForm.addEventListener("submit", login);
+addMessageForm.addEventListener("submit", sendMessage)
